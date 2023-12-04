1683ac6aee51ef0188045a22b31d806b20233b6b
GitShafik <152851921+GitShafik@users.noreply.github.com>
2023-12-04 14:48:45 +0100

    update2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("hi Fikret and Tommis");
         System.out.println("Hello guys!");
+
+        GitCode gitCode = new GitCode();
+        System.out.println(gitCode);
+    }
+
+    public class GitCode{
+
+        String [] list = new String[]{"Tommis, Fikret, shafik"};
+
     }
 }
